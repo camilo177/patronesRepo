a9ec858948d72b6f6e00f4782fa001fce7a21221
Juan Camilo Lopez Caicedo <juan.lopezca@campusucc.edu.co>
2023-11-15 18:40:03 -0500

    trabajo

--- src/bankPkg/Banco.java
+++ src/bankPkg/Banco.java
@@ -1,6 +1,5 @@
 package bankPkg;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,16 +9,11 @@ public class Banco {
     public Banco() {
         cuentas = new ArrayList<>();
     }
-
     public List<Cuenta> getCuentas() {
         return cuentas;
     }
     public void setCuentas(List<Cuenta> cuentas) {
         this.cuentas = cuentas;
-    }
-    public void transferir(Cuenta origen, Cuenta destino, BigDecimal monto) {
-        origen.debito(monto);
-        destino.credito(monto);
     }
     public void anadirCuenta(Cuenta cuenta) {
         this.cuentas.add(cuenta);
